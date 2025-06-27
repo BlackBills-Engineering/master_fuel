2a3ad7c2c25547fcc437a14506e7f2b0249bf64d
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2025-06-28 00:18:19 +0500

    init

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@ class Settings(BaseModel):
     serial_port: str = Field("/dev/ttyS0")
     baud_rate:  int  = Field(19200)
     poll_interval: float = 0.03           # 30 мс
-    addr_start: int = Field(0x01)   # диапазон адресов
-    addr_end:   int = Field(0x20)     # включительно
+    addr_start: int = Field(0x52)   # диапазон адресов
+    addr_end:   int = Field(0x52)     # включительно
 
 @lru_cache
 def get_settings() -> Settings:
