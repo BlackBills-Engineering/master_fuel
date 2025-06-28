050bb168f76cbfb6611d2678d423c22971752f72
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2025-06-28 14:50:14 +0500

    init

--- app/quick_poll.py
+++ app/quick_poll.py
@@ -1,5 +1,5 @@
 import serial, time, binascii, sys
-port = "/dev/ttyS4"    # ← твой
+port = "/dev/ttyS5"    # ← твой
 ser  = serial.Serial(port, 19200, bytesize=8,
                      parity=serial.PARITY_ODD, stopbits=1)
 addr = 0x52             # попробуй 0x50..0x55 и 0x01..0x05
