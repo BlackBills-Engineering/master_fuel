4ce4b71e0db07ca58fda0ce197aa1d79a1e1673c
mur073 <murodzon104@gmail.com>
2025-06-29 18:40:38 +0500

    meow

--- app/enums.py
+++ app/enums.py
@@ -1,20 +1,20 @@
 from enum import IntEnum
 
 class PumpCmd(IntEnum):
-    RESET      = 0x01
-    STOP       = 0x02
-    AUTHORIZE  = 0x06
-    SUSPEND    = 0x0A
-    RESUME     = 0x0B
-    SWITCH_OFF = 0x05
+    RETURN_STATUS       = 0x00      # 0H RETURN STATUS
+    RETURN_PUMP_PARAMS  = 0x02      # 2H RETURN PUMP PARAMETERS (*)
+    RETURN_PUMP_IDENTITY = 0x03     # 3H RETURN PUMP IDENTITY
+    RETURN_FILLING_INFO = 0x04      # 4H RETURN FILLING INFORMATION
+    RESET               = 0x05      # 5H RESET
+    AUTHORIZE           = 0x06      # 6H AUTHORIZE
+    STOP                = 0x08      # 8H STOP
+    SWITCH_OFF          = 0x0A      # AH SWITCH OFF
 
 class PumpStatus(IntEnum):          # DC1 «RETURN STATUS»
-    IDLE                = 0x00      # ← колонка шлёт именно его
-    RESET               = 0x01
-    AUTHORIZED          = 0x02
-    FILLING             = 0x03
-    SUSPENDED           = 0x04
-    FILLING_COMPLETED   = 0x05
-    MAX_REACHED         = 0x06
-    SWITCHED_OFF        = 0x07
-    PUMP_NOT_PROGRAMMED = 0x08
+    PUMP_NOT_PROGRAMMED = 0x00      # 0 PUMP NOT PROGRAMMED
+    RESET               = 0x01      # 1 RESET
+    AUTHORIZED          = 0x02      # 2 AUTHORIZED
+    FILLING             = 0x04      # 4 FILLING
+    FILLING_COMPLETED   = 0x05      # 5 FILLING COMPLETED
+    MAX_REACHED         = 0x06      # 6 MAX AMOUNT/VOLUME REACHED
+    SWITCHED_OFF        = 0x07      # 7 SWITCHED OFF
