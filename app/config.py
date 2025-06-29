e1c2d66d970658a70bf789ed0b218cda3f0f920c
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2025-06-29 15:35:21 +0500

    init

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@ class Settings(BaseModel):
     serial_port: str = Field("COM3")
     baud_rate:  int  = Field(9600)
     poll_interval: float = 0.03           # 30 мс
-    addr_start: int = Field(0x52)   # диапазон адресов
-    addr_end:   int = Field(0x52)     # включительно
+    addr_start: int = Field(0x50)   # диапазон адресов
+    addr_end:   int = Field(0x50)     # включительно
 
 @lru_cache
 def get_settings() -> Settings:
