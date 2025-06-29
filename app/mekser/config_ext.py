9bc8d21080756569dacfaf2936d911795d94dc18
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2025-06-29 20:21:08 +0500

    init

--- app/mekser/config_ext.py
+++ app/mekser/config_ext.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 class DriverCfg(BaseModel):
     serial_port: str = Field("COM3")   # Windows COM-порт
     baud_rate:   int  = Field(9600)
-    parity:      str  = Field("N")        # «O», «E», «N»
+    parity:      str  = Field("O")        # «O», «E», «N»
     timeout:     float= Field(0.5)
     crc_init: int = 0x0000
     crc_poly: int = 0x1021
